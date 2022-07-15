93551b6f9685fba7740df98fc15d97fe13a7b194
LeandroHurtado <leandro_david@hotmail.es>
2022-07-15 18:52:11 -0300

    fix: error fixed

--- src/shippers/shippers.module.ts
+++ src/shippers/shippers.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ShippersService } from './shippers.service';
 import { ShippersController } from './shippers.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Shipper } from './entities/shippers.entities';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Shipper])],
   providers: [ShippersService],
-  controllers: [ShippersController]
+  controllers: [ShippersController],
+  exports: [TypeOrmModule],
 })
 export class ShippersModule {}
