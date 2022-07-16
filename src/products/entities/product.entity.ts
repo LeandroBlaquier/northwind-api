e48ffab46381db06d020704d3ad8853ffb3eb932
nacho-bootcamp <ignaciocardozoxd12@gmail.com>
2022-07-15 21:08:23 -0300

    feat: relations

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { suppliers } from 'src/suppliers/entities/suppliers.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
@@ -31,4 +32,7 @@ export class Product {
 
   @Column()
   discontinuedt: number;
+
+  @ManyToOne(() => suppliers, (suppliers) => suppliers.products)
+  product: Product;
 }
